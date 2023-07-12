5350700066d5154391acb876c26d76ecdf27e38c
Niklas Blanchard <cheapseatsecon@gmail.com>
2023-07-12 08:24:26 -0500

    documentation

--- index.js
+++ index.js
@@ -11,21 +11,52 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+/**
+ * Default route. Redirects to decode_csr route.
+ * 
+ * @name GET/
+ * @function
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ */
 app.get('/', (req, res) => {
     res.writeHead(301, { Location: "https://localhost:8443/decode_csr" });
     res.end();
 });
 
+/**
+ * Route to serve decode_csr page.
+ * 
+ * @name GET/decode_csr
+ * @function
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ */
 app.get('/decode_csr', (req, res) => {
     //serve webpage
     res.sendFile('decode_csr.html', { root: __dirname })
 });
 
+/**
+ * Route to serve decode_csr documentation page.
+ * 
+ * @name GET/decode_csr/docs
+ * @function
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ */
 app.get('/decode_csr/docs', (req, res) => {
     res.sendFile('decode_csr_docs.html', { root: __dirname })
 });
 
+/**
+ * Post route for decoding CSR.
+ * 
+ * @name POST/decode_csr
+ * @function
+ * @param {object} req - Express request object. Should contain CSR in body.
+ * @param {object} res - Express response object
+ */
 app.post('/decode_csr', (req, res) => {
     try {
         const csr = req.body.csr;
@@ -68,7 +99,21 @@ const httpsOptions = {
     cert: fs.readFileSync('./certstore/server.crt', 'utf-8')
 };
 
+/**
+ * HTTPS Server
+ * 
+ * @type {https.Server}
+ */
 const server = https.createServer(httpsOptions, app);
 
+/**
+ * Server port. Defaults to 443.
+ * 
+ * @type {number}
+ */
 const port = process.env.PORT || 443;
+
+/**
+ * Starts the server on the defined port.
+ */
 server.listen(port, () => console.log(`HTTPS Server is listening on port ${port}`));
